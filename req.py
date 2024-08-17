3e4b3a507b69faac5da8e9176614573e226e05df
rianaditro <rianaditro@gmail.com>
2024-08-17 12:53:14 +0700

    initial commit

---
+++ req.py
@@ -0,0 +1,24 @@
+import requests
+
+
+
+if __name__ == "__main__":
+    shopList = [{"name":"sovlo",
+                 "products": 507,
+                 "page":7,
+                 "url":"https://www.tokopedia.com/sovlo/product"},
+                 {"name":"hawman",
+                  "products":27,
+                  "page":1,
+                  "url":"https://www.tokopedia.com/hawman/product"},
+                  {"name":"kamalika_artprints",
+                   "products":1588,
+                   "page":20,
+                   "url":"https://www.tokopedia.com/kamalikaartprints/product/"},
+                   {"name":"pamole",
+                    "products":181,
+                    "page":3,
+                    "url":"https://www.tokopedia.com/pamole/product/"}
+                  ]
+    
+    productList = ["https://www.tokopedia.com/tonga-bags/tas-belanja-foldable-shopping-bag-foldable-sablon-new-pattern-colourfullpaint-f0103?"]
